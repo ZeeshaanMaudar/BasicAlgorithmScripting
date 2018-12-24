40c5311c286d1cb55bed735368c623b7498f286b
Zeeshaan <maudarbocus.zeeshaan@gmail.com>
2018-12-24 21:12:22 +0200

    refactoring code to better alternative using spread operator

--- SliceAndSplice/solution.js
+++ SliceAndSplice/solution.js
@@ -1,10 +1,7 @@
 function frankenSplice(arr1, arr2, n) {
   // It's alive. It's alive!
   let newArr2 = arr2.slice();
-  for (let i = 0; i < arr1.length; i++) {
-    newArr2.splice(n, 0, arr1[i]);
-    n++;
-  }
+  newArr2.splice(n, 0, ...arr1);
   return newArr2;
 }
 
